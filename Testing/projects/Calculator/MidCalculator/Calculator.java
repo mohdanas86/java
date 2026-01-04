0fa03ca862ac6f72d94c3abb55df9badaaaacdfd
mohdanas86 <anasalam7739@gmail.com>
2026-01-04 15:52:44 +0530

    Add Calculator.java with methods for basic arithmetic operations: addition, subtraction, multiplication, and division

---
+++ Testing/projects/Calculator/MidCalculator/Calculator.java
@@ -0,0 +1,19 @@
+package Testing.projects.Calculator.MidCalculator;
+
+public class Calculator {
+    double addition(double a, double b) {
+        return a + b;
+    }
+
+    double substraction(double a, double b) {
+        return a - b;
+    }
+
+    double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    double division(double a, double b) {
+        return a / b;
+    }
+}
