6f309fc3d77747677a15870133d345c6fdcce670
mohdanas86 <anasalam7739@gmail.com>
2026-01-05 17:51:48 +0530

    Add EasyCal and CalculatorApp classes for interactive calculator functionality

---
+++ Testing/projects/Medium/MidCalculator/Calculator.java
@@ -0,0 +1,19 @@
+package Testing.projects.Medium.MidCalculator;
+
+public class Calculator {
+    double addition(double a, double b) {
+        return a + b;
+    }
+
+    double substraction(double a, double b) {
+        return a - b;
+    }
+
+    double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    double division(double a, double b) {
+        return a / b;
+    }
+}
